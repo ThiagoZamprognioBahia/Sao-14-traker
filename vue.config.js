040468b91f9ce8a0f8aece5cdd019a320c7f7e2a
Thiago <zcranigan11@gmail.com>
2023-08-24 10:25:11 -0300

    Ajuste de rotas para o gitpages

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/Sao-14-traker/' : '/',
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
